d7f609d5ef7ce28abf6df33fb2b0f4ac332081a8
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-09-05 17:03:15 +0500

    Initial Commit

---
+++ TravelAppUI/TravelAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppUIApp.swift
+//  TravelAppUI
+//
+//  Created by Muhammad Khan on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
